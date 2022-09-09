fd72ff988c78ceeae9c4ea3a13010a805f69263d
iAhmed3v <devahmdali@gmail.com>
2022-09-09 17:42:01 +0300

    update OrderViewModel.kt

--- app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
+++ app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
@@ -71,12 +71,20 @@ class OrderViewModel : ViewModel() {
     fun setEntree(entree: String) {
         // TODO: if _entree.value is not null, set the previous entree price to the current
         //  entree price.
+        if(_entree.value != null){
+            previousEntreePrice = entree.toDouble()
+        }
 
         // TODO: if _subtotal.value is not null subtract the previous entree price from the current
         //  subtotal value. This ensures that we only charge for the currently selected entree.
+        if (_subtotal.value != null){
+            _subtotal.value!!.minus(previousEntreePrice)
+        }
 
         // TODO: set the current entree value to the menu item corresponding to the passed in string
+        _entree.value = menuItems[entree]
         // TODO: update the subtotal to reflect the price of the selected entree.
+        updateSubtotal(entree.toDouble())
     }
 
     /**
